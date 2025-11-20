0606640d22d27caefbfd5dcf5307a56a74cac1cf
JMSorucoS <120837520+JMSorucoS@users.noreply.github.com>
2025-11-20 08:02:10 -0400

    Update SongNFT.sol

--- contracts/SongNFT.sol
+++ contracts/SongNFT.sol
@@ -12,22 +12,16 @@ contract SongNFT is ERC721, Ownable {
         string memory _name,
         string memory _symbol,
         string memory _metadataURI,
-        address _initialOwner // Cambié el nombre de la variable para ser más claro
+        address _initialOwner
     ) 
-        // Llama al constructor de ERC721 con nombre y símbolo
         ERC721(_name, _symbol) 
-        // Llama al constructor de Ownable con el propietario inicial
         Ownable(_initialOwner) 
     {
         metadataURI = _metadataURI;
-        // Se eliminó transferOwnership(_owner) ya que Ownable(_initialOwner) lo configura
     }
 
-    // Nota: Es mejor usar external/public y no onlyOwner aquí, 
-    // ya que el Factory debería ser el propietario y la función mint es llamada por el Factory
     function mint(address to) public onlyOwner returns (uint256) {
         uint256 id = tokenIdCounter;
-        // Solo mintear si el token no existe, aunque el contador garantiza que es único
         _safeMint(to, id); 
         tokenIdCounter += 1;
         return id;
@@ -36,4 +30,5 @@ contract SongNFT is ERC721, Ownable {
     function tokenURI(uint256) public view override returns (string memory) {
         return metadataURI;
     }
+
 }
